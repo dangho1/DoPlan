022f0661053a1b8d964c910749f53f9fc55ba3ea
dangho1 <dangho98@gmail.com>
2026-07-03 23:06:17 +0200

    Bump version

--- app-version.js
+++ app-version.js
@@ -1,9 +1,9 @@
 /** Github build action for new app versions only trigger on changes in this file,
  * this is why these versions are isolated here.
  *  */
-/* eslint-disable no-undef */
+
 module.exports = {
   version: "1.0.0",
-  iosBuildNumber: "4",
+  iosBuildNumber: "5",
   androidVersionCode: 2,
 };
